1b387d47c52504ef9537efa7376ca2147a947a89
David Morrill <judimorrill@Judis-MacBook-Pro.local>
2018-03-29 00:48:30 -0700

    here we go

---
+++ app/utils/helpers.js
@@ -0,0 +1,25 @@
+const Helpers = {
+    getArticles: (topic, startYear, endYear) => {
+        var authKey = "568297530230411eade03711b5fe7fcc";
+        var queryURLBase = "https://api.nytimes.com/svc/search/v2/articlesearch.json?api-key=" + authKey + "&q=";
+        var queryURL = queryURLBase + topic;
+        if (parseInt(startYear)) {
+            queryURL = queryURL + "&begin_date=" + startYear + "0101";
+        }
+        if (parseInt(endYear)) {
+            queryURL = queryURL + "&end_date=" + endYear + "0101";
+        }
+        return axios.get(queryURL);
+    },
+    saveArticle: (title, url) => {
+        return axios.post('/api/saved', { title: title, url: url });
+    },
+    getSaved: () => {
+        return axios.get('/api/saved');
+    },
+    deleteArticle: (id) => {
+        return axios.delete(`/api/saved/${id}`);
+    },
+};
+
+export default Helpers;
